18d7da7cdd55d66f7448580126b05b88099acc53
leonnariley18-ui <leonnariley18@gmail.com>
2026-07-18 08:48:19 +0000

    Switch to /docs build output, stop overwriting source index.html

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/the-cloud-V2/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
